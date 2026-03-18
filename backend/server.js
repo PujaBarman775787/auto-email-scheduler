ec02b761692903e4d7e01a308c122674c1e6fc9d
PujaBarman775787 <pujabarman13572gmail.com>
2026-03-18 12:58:37 +0530

    frontend deploy fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+// backend/server.js
+// server.js
 import app from "./src/app.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-if (process.env.VERCEL_ENV === undefined) {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});

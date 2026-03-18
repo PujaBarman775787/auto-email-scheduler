d57b6d7ffb85e3d0b5f04d12cd303d32c8096243
PujaBarman775787 <pujabarman13572gmail.com>
2026-03-18 13:16:33 +0530

    connect backend live url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://auto-email-scheduler-9zx6.onrender.com/api",
 });
 
 export default api;

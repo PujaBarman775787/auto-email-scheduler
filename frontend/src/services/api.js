9f5760b1816765bef45344fbde431c3041f3462d
PujaBarman775787 <pujabarman13572gmail.com>
2026-03-18 13:20:24 +0530

    fix https api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://auto-email-scheduler-9zx6.onrender.com/api",
+  baseURL:  "https://auto-email-scheduler-9zx6.onrender.com/api"
 });
 
 export default api;
